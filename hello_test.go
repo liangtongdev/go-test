9b4e408cad6374c62ccddf3fa8c2f3f629822677
liangtong <liangtongdev@163.com>
2019-11-13 10:07:37 +0800

    go mod test

---
+++ hello_test.go
@@ -0,0 +1,9 @@
+package hello
+import "testing"
+
+func TestHello(t *testing.T) {
+    want := "Hello, world."
+    if got := Hello(); got != want {
+        t.Errorf("Hello() = %q, want %q", got, want)
+    }
+}
